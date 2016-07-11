9c5ebe30481089a29cf4602782443917a1442294
Paoola <paola.mauceri7@gmail.com>
2016-07-11 17:18:14 +0200

    searchbytag

--- src/EDR/AppliBundle/Controller/AppliController.php
+++ src/EDR/AppliBundle/Controller/AppliController.php
@@ -3,18 +3,26 @@
 namespace EDR\AppliBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EDR\AppliBundle\Entity\Tag;
+use Symfony\Component\HttpFoundation\Request;
 
 class AppliController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
         $categories = $em->getRepository('EDRAppliBundle:Categorie')->findAll();
-        
+
+        $form = $this->createForm('EDR\AppliBundle\Form\EtabFindByTagType');
+        $id = $request->request->get('etab_find_by_tag')['nom'];
+
+        $etablissements = $em->getRepository('EDRAppliBundle:Etablissement')->findBy(array('id' => $id));
+         
         return $this->render('EDRAppliBundle:Appli:index.html.twig', array(
-            'categories' => $categories
+            'categories' => $categories,
+            'etablissements' => $etablissements,
+            'form' => $form->createView()
         ));
-        
     }
 }
